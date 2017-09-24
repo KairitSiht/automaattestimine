7e09c5d623a3a74233b462bec7ccfdf0faaa9efb
Kairit <kairit.siht@outlook.com>
2017-09-24 15:23:19 +0300

    Add files via upload

---
+++ WeatherApp/src/test/java/Test1/JUnit.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kairit
+ */
+public class JUnit {
+    
+    public JUnit() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void hello() { fail();}
+    public void isTemperatureInNormalValues(){}
+    public void isTheHTTPStautusOK(){}
+    public void isTheFormatJSON(){}
+    public void areGEOValuesInAPromisedWay(){}
+    public void isTheLowestReallyTheLowestTemp(){}
+    public void isTheHighestReallyTheHighestTemp(){}
+    public void isinfoGivenForThreeDaysOnly(){}
+    
+}
